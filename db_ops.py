22e4808d1d29e34051da26c5aef5c6317817ba6e
dike <dike@dikekalu.com>
2015-02-28 10:29:41 -0500

    done

--- db_ops.py
+++ db_ops.py
@@ -1,10 +1,14 @@
 from math import radians, cos, sin, asin, sqrt
 
-from models import Physicians, Tract, WithinMiles
+from models import Physicians, Tract, WithinMiles, TractCode
 
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.sql.expression import func
+from sqlalchemy.sql import label
+import requests
+
 
 engine = create_engine("postgresql+psycopg2://dkalu:nigeria@localhost/docsdb")
 Session = sessionmaker(bind=engine)
@@ -132,28 +136,223 @@ def populate_distance_table():
 
 
 def delete_all():
-    tracts = session.query(Tract).all()
-    physicians = session.query(Physicians).all()
-    miles = session.query(WithinMiles).all()
-    print 'starting tracts'
-    for tract in tracts:
-        session.delete(tract)
-    print 'starting physicians'
-    for physician in physicians:
-        session.delete(physician)
-    print 'starting miles'
-    for mile in miles:
-        session.delete(mile)
+    print session.query(Tract).delete()
+    print session.query(Physicians).delete()
+    #tracts = session.query(Tract).all()
+    #physicians = session.query(Physicians).all()
+    #miles = session.query(WithinMiles).all()
+    #print 'starting tracts'
+    #for tract in tracts:
+    #    session.delete(tract)
+    #print 'starting physicians'
+    #for physician in physicians:
+    #    session.delete(physician)
+    #print 'starting miles'
+    #for mile in miles:
+    #    session.delete(mile)
     session.commit()
+
+
+def _six_digits(value):
+    value = str(int(value))
+    while len(value) < 6:
+        value = '0' + value
+    return value
+
+
+def get_tract_code_id(tract_code):
+    try:
+        atract_code = session.query(TractCode).filter_by(tract_code=tract_code).one()
+        return_obj = atract_code.id
+    except:
+        new = TractCode(tract_code=tract_code)
+        session.add(new)
+        session.commit()
+        return_obj = new.id
+    finally:
+        return return_obj
+
+
+def populate_tract_code():
+    count = 0
+    tracts = session.query(Tract).all()
+    for tract in tracts:
+        print count
+        count += 1 
+        tract_code = tract.state_fp + tract.county_fp + tract.tract_ce
+        tract.tract_code_id = get_tract_code_id(tract_code)    
+    session.commit()
+
+def get_physician_tract_codes():
+    count = 0
+    for doc in session.query(Physicians).all():
+        print count
+        errors = []
+        count += 1
+        url = "http://data.fcc.gov/api/block/find?format=json&latitude=%s&longitude=%s&showall=false" % (doc.latitude_p, doc.longitude_p)
+        r = requests.get(url)
+        if r.status_code != 200:
+            print 'NOOOOOOOOOOOOOOOOO'
+        
+        try:
+            tract_code = r.json()['Block']['FIPS'][:-4] 
+            doc.tract_code_id = get_tract_code_id(tract_code)
+        except:
+            print r.json()
+            errors.append(r.json())
+
+    session.commit()
+    print errors
+    print 'all done with api call' 
+
+
+def calc_counts(threshold=30):
+    docs =  session.query(Physicians).all()
+    tracts = session.query(Tract).all()
+    count = 0
+
+    for doc in docs:
+        doc_count = 0
+        tract_count = 0
+        lat = doc.latitude_p
+        lon = doc.longitude_p
+        
+        for adoc in docs:
+            if haversine(lon, lat, adoc.longitude_p, adoc.latitude_p) <= threshold:
+                doc_count += 1
+        doc.docs_within = doc_count
+
+        for tract in tracts:
+            if haversine(lon, lat, tract.longitude_t, tract.latitude_t) <= threshold:
+                tract_count += 1
+        doc.tracts_within = tract_count
+        session.commit()
+        print count
+        count += 1
+    print 'done with calc counts'
+
+
+def calc_countss():
+    docs =  session.query(Physicians).all()
+    tracts = session.query(Tract).all()
+    count = 0
+
+    for doc in docs:
+        doc_count_10 = 0
+        doc_count_15 = 0
+        doc_count_45 = 0
+        doc_count_60 = 0
+
+        tract_count_10 = 0
+        tract_count_15 = 0
+        tract_count_45 = 0
+        tract_count_60 = 0
+
+        lat = doc.latitude_p
+        lon = doc.longitude_p
+
+        for adoc in docs:
+            if haversine(lon, lat, adoc.longitude_p, adoc.latitude_p) <= 10:
+                doc_count_10 += 1
+
+            if haversine(lon, lat, adoc.longitude_p, adoc.latitude_p) <= 15:
+                doc_count_15 += 1
+
+            if haversine(lon, lat, adoc.longitude_p, adoc.latitude_p) <= 45:
+                doc_count_45 += 1
+
+            if haversine(lon, lat, adoc.longitude_p, adoc.latitude_p) <= 60:
+                doc_count_60 += 1
+
+        doc.docs_within_10 = doc_count_10
+        doc.docs_within_15 = doc_count_15
+        doc.docs_within_45 = doc_count_45
+        doc.docs_within_60 = doc_count_60
+
+        for tract in tracts:
+            if haversine(lon, lat, tract.longitude_t, tract.latitude_t) <= 10:
+                tract_count_10 += 1
+
+            if haversine(lon, lat, tract.longitude_t, tract.latitude_t) <= 15:
+                tract_count_15 += 1
+
+            if haversine(lon, lat, tract.longitude_t, tract.latitude_t) <= 45:
+                tract_count_45 += 1
+
+            if haversine(lon, lat, tract.longitude_t, tract.latitude_t) <= 60:
+                tract_count_60 += 1
+
+        doc.tracts_within_10 = tract_count_10
+        doc.tracts_within_15 = tract_count_15
+        doc.tracts_within_45 = tract_count_45
+	doc.tracts_within_60 = tract_count_60
+
+        session.commit()
+        print count
+        count += 1
+    print 'done with calc counts'
+
+def calc_ratio(distance):
+    attr_map = {
+	10: ['docs_within_10', 'tracts_within_10', 'ratio_10'],
+	15: ['docs_within_15', 'tracts_within_15', 'ratio_15'],
+        30: ['docs_within', 'tracts_within', 'ratio_30'],
+	45: ['docs_within_45', 'tracts_within_45', 'ratio_45'],
+	60: ['docs_within_60', 'tracts_within_60', 'ratio_60']
+	}
+
+    docs = session.query(Physicians).all()
+
+    calc = attr_map[distance]
+    count = 0
+    for doc in docs:
+        sum_physicians = getattr(doc, calc[0])
+        #sum_pop = session.query(func.sum(WithinMiles.population)).filter(WithinMiles.miles<=distance, WithinMiles.object_id==doc.object_id)
+        #sum_pop = session.query(WithinMiles, label('sum', func.sum(WithinMiles.population)), label('count', func.count(WithinMiles.id))).filter(WithinMiles.miles<=distance, WithinMiles.object_id==doc.object_id).all()
+        #sum_pop = session.query(func.sum(WithinMiles.population).label('sum_pop')).filter(WithinMiles.miles<=distance, WithinMiles.object_id==doc.object_id)
+        all = session.query(WithinMiles).filter(WithinMiles.miles<=distance, WithinMiles.object_id==doc.object_id)
+        sum_pop = sum(int(a.population) for a in all)
+        
+        if sum_pop > 0:
+            ratio = float(sum_physicians) / float(sum_pop)
+        else:
+            ratio = 0
+        setattr(doc, calc[2], ratio)
+	print "count(%s): %s    ratio: %s" % (distance, count, ratio)
+        count += 1
+    session.commit()
+
+
+        #print dir(sum_pop)
+        #print sum_pop.execute()
+        #return
+        #ratio = sum_physicians / sum_pop
+        #setattr(doc, calc[2], ratio)
+        #print sum_physicians, sum_pop, ratio
+        #count = session.query(WithinMiles).filter(WithinMiles.miles<=distance, WithinMiles.object_id==doc.object_id).count()
+        #print '------'
+        #print count, getattr(doc, calc[1])
+        #return
 
 # [48.0, 1.0, 950401.0, 5422.0, 31.755614, -95.823901]
 # [14178, -95.444343, 30.129775, 'SPRING', 'TX', 77380]
 # 28.710983 -99.829416
 # 31.99936,-95.53183
 if __name__ == '__main__':
-    # delete_all()
-    # print haversine('-99.829416', '28.710983', '-95.53183', '31.99936')
-    unpack_centroids()
-    unpack_physicians()
-    populate_distance_table()
-    print 'All done'
+    #delete_all()
+    #print haversine('-99.829416', '28.710983', '-95.53183', '31.99936')
+    #unpack_centroids()
+    #unpack_physicians()
+    #populate_distance_table()
+    #a = session.query(Tract).limit(0).first()
+    #print a.tract_code_id
+    #populate_tract_code()
+    #get_physician_tract_codes()
+    #calc_countss()
+    calc_ratio(30)
+    calc_ratio(15)
+    calc_ratio(10)
+    calc_ratio(45)
+    calc_ratio(60)
+    #print session.query(Physicians).all()[0].docs_within
+    #print session.query(Physicians).all()[0].tracts_within
--- models.py
+++ models.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, Integer, String  # , DateTime, Table
+from sqlalchemy import Column, Float, Integer, String, ForeignKey  # , DateTime, Table
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+class TractCode(Base):
+    __tablename__ = 'tract_code'
+
+    id = Column(Integer, primary_key=True)
+    tract_code = Column(String)
+    tracts = relationship('Tract', backref='tract_code')
+    physicians = relationship('Physicians', backref='tract_code')
+    two_stage = Column(Float)
 
 class Tract(Base):
     __tablename__ = 'tract'
@@ -16,6 +23,7 @@ class Tract(Base):
     population = Column(String)
     latitude_t = Column(String)
     longitude_t = Column(String)
+    tract_code_id = Column(Integer, ForeignKey('tract_code.id'))
 
     def _three_digits(self, value):
         value = str(int(value))
@@ -34,7 +42,22 @@ class Physicians(Base):
     city = Column(String)
     state = Column(String)
     zipcode = Column(String)
-
+    tract_code_id = Column(Integer, ForeignKey('tract_code.id'))
+    docs_within = Column(Integer)
+    docs_within_10 = Column(Integer)
+    docs_within_15 = Column(Integer)
+    docs_within_45 = Column(Integer)
+    docs_within_60 = Column(Integer)
+    tracts_within = Column(Integer)
+    tracts_within_10 = Column(Integer)
+    tracts_within_15 = Column(Integer)
+    tracts_within_45 = Column(Integer)
+    tracts_within_60 = Column(Integer)
+    ratio_10 = Column(Float)
+    ratio_15 = Column(Float)
+    ratio_30 = Column(Float)
+    ratio_45 = Column(Float)
+    ratio_60 = Column(Float)
 
 class WithinMiles(Base):
     __tablename__ = 'within_miles'
